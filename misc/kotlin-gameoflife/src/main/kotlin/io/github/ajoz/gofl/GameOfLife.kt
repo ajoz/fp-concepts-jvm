822cf8aba6676a2cdf977741c0e3c348ecab747a
Andrzej Jozwiak <andrzej.jozwiak@gmail.com>
2018-06-20 08:37:58 +0200

    [kotlin] Reworking game of life code

--- misc/kotlin-gameoflife/src/main/kotlin/io/github/ajoz/gofl/GameOfLife.kt
+++ misc/kotlin-gameoflife/src/main/kotlin/io/github/ajoz/gofl/GameOfLife.kt
@@ -12,60 +12,62 @@ package io.github.ajoz.gofl
 // Do we really need to create a specialized class right from the start?
 // data class Position(val x: Int, val y: Int)
 // maybe we can just do a simplification
-typealias Position = Pair<Int, Int>
+private typealias Position = Pair<Int, Int>
 
 // would be nice to use more domain specific names instead of first and second
-val Position.x: Int
+private val Position.x: Int
     get() = first
 
-val Position.y: Int
+private val Position.y: Int
     get() = second
 
 // if the cell does not have any other properties then being alive it means that essentially its a position in the world
 // but in certain context its better to think about it using the word Cell not a word Position
-typealias Cell = Position
+private typealias Cell = Position
 
 // One generation of live cells is just basically a list, but a word List brings different properties with itself
 // its better to think about a generation, list is an implementation detail
-typealias Generation = Collection<Cell>
+private typealias Generation = Collection<Cell>
 
 // we want to change each generation into next generation, we need a set of candidates for it
 // the word candidates brings our intention with it, it also expresses our uncertainty about it beeing a cell or not
 // it's basically a position but reads better as Candidate in correct context
-typealias Candidate = Position
+private typealias Candidate = Position
 
 // we want to hide the detail of how candidates are stored, also we want to express that we return multiple instances of
 // a candidate, we don't need to know its a list or any other data structure
-typealias Candidates = Collection<Candidate>
+private typealias Candidates = Collection<Candidate>
 
 // cell has several positions around it, those positions are it's neighbours. This word brings familiarity of the concept
 // we all have neighbours, it describes where the thing is without any values, you feel that it means something that is
 // close
-typealias Neighbour = Position
-typealias Neighbours = Collection<Neighbour>
+private typealias Neighbour = Position
+
+private typealias Neighbours = Collection<Neighbour>
 
 // maybe its a wrong word, in physics it means how many times per second a thing occurs
 // here we would like to express the NeighbourCount for a particular Candidate
-typealias Frequency = Pair<Candidate, NeighbourCount>
-typealias NeighbourCount = Int
+private typealias Frequency = Pair<Candidate, NeighbourCount>
 
-val Frequency.candidate: Candidate
+private typealias NeighbourCount = Int
+
+private val Frequency.candidate: Candidate
     get() = first
 
-val Frequency.neighbourCount: NeighbourCount
+private val Frequency.neighbourCount: NeighbourCount
     get() = second
 
-typealias Frequencies = Collection<Frequency>
+private typealias Frequencies = Collection<Frequency>
 
 // getNeighbours :: Cell -> Neighbours
-fun getNeighbours(cell: Cell): Neighbours = listOf(
+private fun getNeighbours(cell: Cell): Neighbours = listOf(
         Neighbour(cell.x - 1, cell.y + 1), Neighbour(cell.x, cell.y + 1), Neighbour(cell.x + 1, cell.y + 1),
         Neighbour(cell.x - 1, cell.y), /*Neighbour(cell.x, cell.y), */ Neighbour(cell.x + 1, cell.y),
         Neighbour(cell.x - 1, cell.y - 1), Neighbour(cell.x, cell.y - 1), Neighbour(cell.x + 1, cell.y - 1)
 )
 
 // getCandidates :: Generation -> Candidates
-fun  getCandidates(generation: Generation): Candidates =
+private fun getCandidates(generation: Generation): Candidates =
         generation.flatMap { getNeighbours(it) }
 
 // for a set of candidates
@@ -76,26 +78,29 @@ fun  getCandidates(generation: Generation): Candidates =
 //    frequency consists of group size (neighbour count)
 
 // getFrequencies :: Candidates -> Frequencies
-fun getFrequencies(candidates: Candidates): Frequencies =
+private fun getFrequencies(candidates: Candidates): Frequencies =
         candidates.sortedBy(Candidate::hashCode)
-                  .groupBy(Candidate::hashCode)
-                  .map { Frequency(it.value[0], it.value.size) }
+                .groupBy(Candidate::hashCode)
+                .map { Frequency(it.value[0], it.value.size) }
 
 // frequencyToCell :: Frequency -> Cell
-fun frequencyToCell(freq: Frequency): Cell = freq.candidate
+private fun frequencyToCell(freq: Frequency): Cell = freq.candidate
 
 //// isCandidateACell :: Frequency -> Generation -> Boolean
 //fun isCandidateACell(freq: Frequency, generation: Generation): Boolean =
 //        (freq.neighbourCount == 3) || (freq.neighbourCount == 2 && generation.contains(freq.candidate))
 
 // isCandidateACell :: Generation -> Frequency -> Boolean
-fun isCandidateACell(gen: Generation): (Frequency) -> Boolean = { freq ->
+private fun isCandidateACell(gen: Generation): (Frequency) -> Boolean = { freq ->
     (freq.neighbourCount == 3) || (freq.neighbourCount == 2 && gen.contains(freq.candidate))
 }
 
 // a nice example of currying
-fun findAliveCells(generation: Generation): (Frequencies) -> Generation =
+private fun findAliveCells(generation: Generation): (Frequencies) -> Generation =
         { fs -> fs.filter(isCandidateACell(generation)).map { frequencyToCell(it) } }
+
+private fun findAliveCells2(isCandidateACell: (Frequency) -> Boolean): (Frequencies) -> Generation =
+        { fs -> fs.filter(isCandidateACell).map { frequencyToCell(it) } }
 
 // for a generation of cells
 // get all candidates from the cells neighbours
@@ -104,58 +109,49 @@ fun findAliveCells(generation: Generation): (Frequencies) -> Generation =
 // discard other candidates
 
 // getNextGeneration :: Generation -> Generation
-fun getNextGeneration(generation: Generation): Generation {
+private fun getNextGeneration(generation: Generation): Generation {
     val candidates = getCandidates(generation)
     val frequencies = getFrequencies(candidates)
     return findAliveCells(generation)(frequencies)
 }
 
-// power that Kotlin has over what Java can give us:
-infix fun <A, R1, R2> ((A) -> R1).andThen(other: (R1) -> R2): (A) -> R2 = { a ->
-    other(this(a))
+private fun getNextGeneration7(generation: Generation, isAlive: (Frequency) -> Boolean = isCandidateACell(generation)): Generation {
+    // generation |> (getCandidates >> getFrequencies >> (findAliveCells2 isAlive))
+    return generation pipeTo (::getCandidates andThen ::getFrequencies andThen findAliveCells2(isAlive))
 }
 
-fun getNextGeneration2(generation: Generation): Generation =
+//
+// candidates <- getCandidates(previous)
+// frequencies <- getFrequencies(candidates)
+// nextGeneration <- findAliveCells(frequencies)
+
+private fun getNextGeneration2(generation: Generation): Generation =
         (::getCandidates andThen ::getFrequencies andThen findAliveCells(generation))(generation)
 
-val getNextGeneration4 = { gen: Generation ->
+private val getNextGeneration4 = { gen: Generation ->
     (::getCandidates andThen ::getFrequencies andThen findAliveCells(gen))(gen)
 }
 
-fun getNextGeneration5(generation: Generation,
-                       findAlive: (Frequencies) -> Generation = findAliveCells(generation)): Generation {
+private fun getNextGeneration5(generation: Generation,
+                               findAlive: (Frequencies) -> Generation = findAliveCells(generation)): Generation {
     return (::getCandidates andThen ::getFrequencies andThen findAlive)(generation)
 }
 
-fun getNextGeneration6(generation: Generation) =
+private fun getNextGeneration6(generation: Generation) =
         findAliveCells(generation)(getFrequencies(getCandidates(generation)))
 
-fun <A, B, T> weave(f: (A) -> B): (T) -> (A) -> B = { g ->
-    { a ->
-        f(a)
-    }
-}
+private fun <A, B> weaveG(f: (A) -> B): (Generation) -> (A) -> B = weave(f)
 
-fun <A, B> weaveG(f: (A) -> B): (Generation) -> (A) -> B = weave(f)
+private val getCandidatesG = weaveG(::getCandidates)
+private val getFrequenciesG = weaveG(::getFrequencies)
+private val findAliveCellsG: (Generation) -> (Frequencies) -> Generation = ::findAliveCells
 
-infix fun <A, B, C, T> ((T) -> (A) -> B).then(f: (T) -> (B) -> C): (T) -> (A) -> C = { g ->
-    { a ->
-        f(g)(this(g)(a))
-    }
-}
-
-fun <T> squish(f: (T) -> (T) -> T): (T) -> (T) = { g -> f(g)(g) }
-
-val getCandidatesG = weaveG(::getCandidates)
-val getFrequenciesG = weaveG(::getFrequencies)
-val findAliveCellsG: (Generation) -> (Frequencies) -> Generation = ::findAliveCells
-
-val getNextGeneration3 =
+private val getNextGeneration3 =
         squish(getCandidatesG then getFrequenciesG then findAliveCellsG)
 
 // impure -- returns Unit, a very broad type would like more precised type like haskell IO
 // printGeneration :: Generation -> IO (String)
-fun printGeneration(generation: Generation): Unit {
+private fun printGeneration(generation: Generation): Unit {
     if (generation.isEmpty()) println("[empty]")
     else {
         val minX = generation.minBy { cell -> cell.x }?.x ?: 0
@@ -177,7 +173,7 @@ fun printGeneration(generation: Generation): Unit {
 }
 
 // stringToGeneration :: String -> Generation
-fun stringToGeneration(string: String): Generation =
+private fun stringToGeneration(string: String): Generation =
         if (string.isBlank()) emptyList()
         else {
             string.split(delimiters = *arrayOf("\n"))
@@ -191,7 +187,7 @@ fun stringToGeneration(string: String): Generation =
 
 // impure but immutable, iteration hidden behind tail recursion
 // runGame :: Generation -> Int -> IO (String)
-tailrec fun runGame(generation: Generation, iterations: Int): Unit =
+private tailrec fun runGame(generation: Generation, iterations: Int): Unit =
         when (iterations) {
             0 -> println("Game finished!")
             else -> {
@@ -202,7 +198,7 @@ tailrec fun runGame(generation: Generation, iterations: Int): Unit =
             }
         }
 
-fun main(args: Array<String>) {
+private fun main(args: Array<String>) {
     val initial =
             """
             _@_
