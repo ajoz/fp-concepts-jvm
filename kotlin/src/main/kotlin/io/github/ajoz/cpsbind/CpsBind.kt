c5f46ed0b4835a2e68b5db8c173aa76e1d300075
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-01-05 00:40:01 +0100

    [kotlin] Continuation passing in case of error and success handling

--- kotlin/src/main/kotlin/io/github/ajoz/cpsbind/CpsBind.kt
+++ kotlin/src/main/kotlin/io/github/ajoz/cpsbind/CpsBind.kt
@@ -1,6 +1,16 @@
 package io.github.ajoz.cpsbind
 
-fun <A, B, C> compose(f: (A) -> B, g: (B) -> C): (A) -> C =
+/*
+  Those pesky Monads are everywhere!
+
+  Let's look at the simple composition of two functions:
+  f :: A -> B
+  g :: B -> C
+
+
+ */
+fun <A, B, C> compose(f: (A) -> B,
+                      g: (B) -> C): (A) -> C =
         { a: A -> g(f(a)) }
 
 val size: (String) -> Int = String::length
@@ -11,6 +21,7 @@ val isStr42 = compose(size, is42)
 typealias SizeContinuation = (Int) -> Unit
 typealias Is42Continuation = (Boolean) -> Unit
 
+// CPS = Continuation Passing Style
 val sizeCPS: (String, SizeContinuation) -> Unit = { str, sc ->
     sc(str.length)
 }
@@ -57,40 +68,42 @@ fun <A, B, C> composeCPSHOF(f: (A) -> (Continuation<B>) -> Unit,
 
 val isStr42HOF = composeCPSHOF(sizeHOF, is42HOF)
 
-class Run<A>(val run: (Continuation<A>) -> Unit)
+class Cont<A>(val run: (Continuation<A>) -> Unit)
 
-val sizeRun: (String) -> Run<Int> = { string ->
-    Run { continuationA: Continuation<Int> -> continuationA(string.length) }
+val sizeRun: (String) -> Cont<Int> = { string ->
+    Cont { continuationA: Continuation<Int> -> continuationA(string.length) }
 }
 
-val is42Run: (Int) -> Run<Boolean> = { int ->
-    Run { it(int == 42) }
+val is42Run: (Int) -> Cont<Boolean> = { int ->
+    Cont { it(int == 42) }
 }
 
-fun <A, B, C> composeRun(f: (A) -> Run<B>,
-                         g: (B) -> Run<C>): (A) -> Run<C> =
+fun <A, B, C> composeCont(f: (A) -> Cont<B>,
+                          g: (B) -> Cont<C>): (A) -> Cont<C> =
         { a: A ->
-            Run { cc ->
+            Cont { cc ->
                 f(a).run { b: B ->
                     g(b).run(cc)
                 }
             }
         }
 
-val isStr42Run = composeRun(sizeRun, is42Run)
+val isStr42Run = composeCont(sizeRun, is42Run)
 
-fun <A, B> bindRun(r: Run<A>, f: (A) -> Run<B>): Run<B> =
-        Run { cb: Continuation<B> ->
+fun <A, B> bindRun(r: Cont<A>, f: (A) -> Cont<B>): Cont<B> =
+        Cont { cb: Continuation<B> ->
             r.run { a: A ->
                 f(a).run(cb)
             }
         }
 
-fun <A, B> Run<A>.bind(f: (A) -> Run<B>): Run<B> =
+fun <A, B> Cont<A>.bind(f: (A) -> Cont<B>): Cont<B> =
         bindRun(this, f)
 
-fun <A> returnRun(value: A): Run<A> =
-        Run { it(value) }
+fun <A> returnRun(value: A): Cont<A> =
+        Cont { it(value) }
+
+
 
 fun main(args: Array<String>) {
     // plain composition
@@ -107,22 +120,22 @@ fun main(args: Array<String>) {
     }
 
     // continuation passing composition with intermediate wrapping type
-    isStr42Run("JUG Łódź CPS HOF Run!").run { isJUGStr42 ->
-        println("Is JUG string size equal 42? HOF - Run style! $isJUGStr42")
+    isStr42Run("JUG Łódź CPS HOF Cont!").run { isJUGStr42 ->
+        println("Is JUG string size equal 42? HOF - Cont style! $isJUGStr42")
     }
 
     // continuation passing composition with intermediate wrapping type bind
     val isStr42Bind = bindRun(
-            sizeRun("JUG Łódź CPS HOF Run with Bind!  fluently!"),
+            sizeRun("JUG Łódź CPS HOF Cont with Bind!  fluently!"),
             is42Run
     )
 
     isStr42Bind.run { isJUGStr42 ->
-        println("Is JUG string size equal 42? HOF - Run style but Bound! $isJUGStr42")
+        println("Is JUG string size equal 42? HOF - Cont style but Bound! $isJUGStr42")
     }
 
     // continuation passing composition with intermediate wrapping type fluently
-    returnRun("JUG Łódź CPS HOF Run with Bind!  fluently!")
+    returnRun("JUG Łódź CPS HOF Cont with Bind!  fluently!")
             .bind(sizeRun)
             .bind(is42Run)
             .run {
@@ -131,10 +144,20 @@ fun main(args: Array<String>) {
 
     // Is it familiar? No?!
 
-    // what if I would rename the Run<A> to Promise<A>
-    // bind to then, run to done?
-
-    // what if I would rename the Run<A> to Observable<A>
+    // what if I would rename the Cont<A> to Observable<A>
     // bind to flatMap, run to subscribe?
+    // you need to squint your eyes cause of lack of error handling
 
+    // what if I would rename the Cont<A> to Maybe<A>
+    // bind to flatMap, run to ifSome?
+    // you need to squint your eyes cause of lack of nothing handling
+
+    // what if I would rename the Cont<A> to Try<A>
+    // bind to flatMap, run to ifSuccess?
+    // you need to squint your eyes cause of lack of failure handling
+
+    // what if I would rename the Cont<A> to Promise<A>
+    // bind to then, run to done?
+    // you need to squint your eyes cause Promise starts processing only after
+    // done is invoked
 }
---
+++ kotlin/src/main/kotlin/io/github/ajoz/cpsbind/CpsErrorHandling.kt
@@ -0,0 +1,150 @@
+package io.github.ajoz.cpsbind
+
+val div42by: (Int, Continuation<Double>, Continuation<String>) -> Unit =
+        { b, onSuccess, onError ->
+            when (b) {
+                0 -> onError("Division by zero!")
+                else -> onSuccess(42.toDouble() / b)
+            }
+        }
+
+val square: (Double, Continuation<Double>, Continuation<String>) -> Unit =
+        { value, onSuccess, onError ->
+            if (value < 0) {
+                onError("Square root of a negative number!")
+            } else {
+                onSuccess(Math.sqrt(value))
+            }
+        }
+
+typealias ResultContinuation<A, B, E> = (A, Continuation<B>, Continuation<E>) -> Unit
+
+fun <A, B, C, E> composeRC(f: ResultContinuation<A, B, E>,
+                           g: ResultContinuation<B, C, E>): ResultContinuation<A, C, E> =
+        { a, onSuccess, onError ->
+            f(a, { b -> g(b, onSuccess, onError) }, onError)
+        }
+
+val divThenSquare = composeRC(div42by, square)
+
+typealias DoubleCont = Continuation<Double>
+typealias StrCont = Continuation<String>
+
+val div42ByCurried: (Int) -> (DoubleCont) -> (StrCont) -> Unit =
+        { b ->
+            { onSuccess ->
+                { onError ->
+                    div42by(b, onSuccess, onError)
+                }
+            }
+        }
+
+val squareCurried: (Double) -> (DoubleCont) -> (StrCont) -> Unit =
+        { value ->
+            { onSuccess ->
+                { onError ->
+                    square(value, onSuccess, onError)
+                }
+            }
+        }
+
+typealias ResultCurried<A, B, E> = (A) -> (Continuation<B>) -> (Continuation<E>) -> Unit
+
+fun <A, B, C, E> composeRCCurried(f: ResultCurried<A, B, E>,
+                                  g: ResultCurried<B, C, E>): ResultCurried<A, C, E> =
+        { a ->
+            { onSuccess ->
+                { onError ->
+                    f(a)() { b ->
+                        g(b)() { c ->
+                            onSuccess(c)
+                        }(onError)
+                    }(onError)
+                }
+            }
+        }
+
+val divThenSquareCurried = composeRCCurried(div42ByCurried, squareCurried)
+
+data class Result1<A, E>(val onSuccess: (Continuation<A>) -> Unit,
+                         val onError: (Continuation<E>) -> Unit)
+
+val div42ByRes1: (Int) -> Result1<Double, String> =
+        { value: Int ->
+            Result1(
+                    onSuccess = { ca -> ca(42.toDouble() / value) },
+                    onError = { ce -> ce("Division by zero!") }
+            )
+        }
+
+val squareRes1: (Double) -> Result1<Double, String> =
+        { value: Double ->
+            Result1(
+                    onSuccess = { ca -> ca(Math.sqrt(value)) },
+                    onError = { ce -> ce("Square root of a negative number!") }
+            )
+        }
+
+typealias Result1Function<A, B, E> = (A) -> Result1<B, E>
+
+fun <A, B, C, E> composeResult1(f: Result1Function<A, B, E>,
+                                g: Result1Function<B, C, E>): Result1Function<A, C, E> =
+        { a ->
+            Result1(
+                    onSuccess = { cc ->
+                        val rb = f(a)
+                        rb.onSuccess { b ->
+                            val rc = g(b)
+                            rc.onSuccess(cc)
+                        }
+                    },
+                    onError = { ce ->
+                        val rb = f(a)
+                        rb.onError(ce)
+                        rb.onSuccess { b ->
+                            val rc = g(b)
+                            rc.onError(ce)
+                        }
+                    }
+            )
+        }
+
+val divThenSquareRes1 = composeResult1(div42ByRes1, squareRes1)
+
+fun main(args: Array<String>) {
+//    divThenSquare(0, { println("divThenSquare success: $it") }, { println("divThenSquare error: $it") })
+//    divThenSquare(-1, { println("divThenSquare success: $it") }, { println("divThenSquare error: $it") })
+//    divThenSquare(42, { println("divThenSquare success: $it") }, { println("divThenSquare error: $it") })
+//
+//    val a = divThenSquareCurried(0)
+//    val b = a {
+//    }
+//    b {
+//    }
+//
+//    divThenSquareCurried(0)() {
+//        println("divThenSquareCurried success: $it")
+//    }() {
+//        println("divThenSquareCurried error: $it")
+//    }
+//
+//    divThenSquareCurried(-1)() {
+//        println("divThenSquareCurried success: $it")
+//    }() {
+//        println("divThenSquareCurried error: $it")
+//    }
+//
+//    divThenSquareCurried(42)() {
+//        println("divThenSquareCurried success: $it")
+//    }() {
+//        println("divThenSquareCurried error: $it")
+//    }
+
+    val res1 = divThenSquareRes1(42)
+    res1.onSuccess { println("divThenSquareRes1 success: $it") }
+    res1.onError { println("divThenSquareRes1 error: $it") }
+
+}
+
+
+
