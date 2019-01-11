ec702cba82cf50e6a88084c178660a7c533ac8f0
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-01-11 14:24:01 +0100

    [java] Pattern Matching in Java, simple and unexhaustive :(

---
+++ java/src/main/java/io/github/ajoz/pattern_matching/PatternMatching.java
@@ -0,0 +1,113 @@
+package io.github.ajoz.pattern_matching;
+
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static io.github.ajoz.pattern_matching.ClassCase.inCaseOf;
+import static io.github.ajoz.pattern_matching.ElseCase.otherwise;
+
+abstract class Foo {
+    public static class Bar extends Foo {
+    }
+
+    public static class Baz extends Foo {
+    }
+}
+
+public class PatternMatching {
+    public static void main(final String[] args) {
+        final Match<Foo, String> matcher =
+                new Match<>(
+                        inCaseOf(Foo.Bar.class, bar -> "bar!"),
+                        inCaseOf(Foo.Baz.class, baz -> "baz!"),
+                        otherwise(foo -> "we don't know!")
+                );
+
+        final Foo foo = new Foo.Baz();
+        matcher
+                .match(foo)
+                .ifPresent(System.out::println);
+    }
+}
+
+class Match<A, B> {
+    private Case<?, B>[] cases;
+
+    @SafeVarargs
+    public Match(final Case<?, B>... cases) {
+        this.cases = cases;
+    }
+
+    public Optional<B> match(final A value) {
+        for (final Case<?, B> c : cases) {
+            if (c.test(value))
+                return c.match(value);
+        }
+
+        return Optional.empty();
+    }
+}
+
+interface Case<A, B> extends Predicate<Object> {
+    default <C> Optional<B> match(final C value) {
+        //noinspection unchecked
+        final A c = (A) value;
+        return runCase(c);
+    }
+
+    Optional<B> runCase(final A value);
+}
+
+final class ClassCase<A, B> implements Case<A, B> {
+    private final Class<A> clazz;
+    private final Function<A, B> function;
+
+    private ClassCase(final Class<A> clazz,
+                      final Function<A, B> function) {
+        this.clazz = clazz;
+        this.function = function;
+    }
+
+    @Override
+    public boolean test(final Object value) {
+        return clazz.isInstance(value);
+    }
+
+    @Override
+    public Optional<B> runCase(final A value) {
+        return Optional.ofNullable(
+                function.apply(value)
+        );
+    }
+
+    public static <A, B> Case<A, B> inCaseOf(final Class<A> clazz,
+                                             final Function<A, B> function) {
+        return new ClassCase<>(clazz, function);
+    }
+}
+
+final class ElseCase<A, B> implements Case<A, B> {
+    private final Function<A, B> function;
+
+    ElseCase(final Function<A, B> function) {
+        this.function = function;
+    }
+
+    @Override
+    public boolean test(Object value) {
+        return true;
+    }
+
+    @Override
+    public Optional<B> runCase(final A value) {
+        return Optional.ofNullable(
+                function.apply(value)
+        );
+    }
+
+    public static <A, B> Case<A, B> otherwise(final Function<A, B> function) {
+        return new ElseCase<>(function);
+    }
+}
