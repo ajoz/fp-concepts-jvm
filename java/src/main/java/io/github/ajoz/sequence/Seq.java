c90b9cda21873b2360be41ed128583ba923b6347
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-03-08 23:57:10 +0100

    [java] Seq concat, flatMap, examples

--- java/src/main/java/io/github/ajoz/sequence/Seq.java
+++ java/src/main/java/io/github/ajoz/sequence/Seq.java
@@ -1,20 +1,56 @@
 package io.github.ajoz.sequence;
 
+import io.github.ajoz.lists.Lists;
+
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public abstract class Seq<A> implements Iterable<A> {
     public abstract A getHead();
+
     public abstract Seq<A> getTail();
 
     public abstract boolean isEmpty();
 
+    public abstract <B> Seq<B> map(final Function<A, B> func);
+
+    public abstract <B> Seq<B> flatMap(final Function<A, Seq<B>> func);
+
     public Seq<A> take(final int threshold) {
         if (threshold == 0)
-            return new Seq.Cons<>(getHead());
+            return new Seq.Cons<>(getHead(), Nil::new);
         else
             return new Seq.Cons<>(getHead(), () -> getTail().take(threshold - 1));
+    }
+
+    public static <A> Seq<A> of(final List<A> list) {
+        if (list.isEmpty()) {
+            return new Seq.Nil<>();
+        } else {
+            final A head = Lists.head(list);
+            final List<A> tail = Lists.tail(list);
+            return new Seq.Cons<>(head, () -> Seq.of(tail));
+        }
+    }
+
+    public static <A> Seq<A> concat(final Seq<A> seq,
+                                    final Supplier<Seq<A>> other) {
+        // if the sequence passed is empty then just return what is in the tail
+        if (seq.isEmpty()) {
+            return other.get();
+            // if the next element of the sequence is empty then we need to concat it
+        } else if (seq.getTail().isEmpty()) {
+            return new Seq.Cons<>(seq.getHead(), other);
+            // if there are more elements to go then just return value and defer
+            // next concat
+        } else {
+            final A head = seq.getHead();
+            final Seq<A> tail = seq.getTail();
+            return new Seq.Cons<>(head, () -> concat(tail, other));
+        }
     }
 
     public static final class Cons<A> extends Seq<A> {
@@ -25,10 +61,6 @@ public abstract class Seq<A> implements Iterable<A> {
                     final Supplier<Seq<A>> tail) {
             this.head = head;
             this.tail = tail;
-        }
-
-        public Cons(final A head) {
-            this(head, Nil::new);
         }
 
         @Override
@@ -64,6 +96,16 @@ public abstract class Seq<A> implements Iterable<A> {
         public boolean isEmpty() {
             return false;
         }
+
+        @Override
+        public <B> Seq<B> map(final Function<A, B> func) {
+            return new Cons<>(func.apply(head), () -> getTail().map(func));
+        }
+
+        @Override
+        public <B> Seq<B> flatMap(final Function<A, Seq<B>> func) {
+            return concat(func.apply(head), () -> getTail().flatMap(func));
+        }
     }
 
     public static final class Nil<A> extends Seq<A> {
@@ -96,6 +138,16 @@ public abstract class Seq<A> implements Iterable<A> {
         @Override
         public boolean isEmpty() {
             return true;
+        }
+
+        @Override
+        public <B> Seq<B> map(Function<A, B> func) {
+            return new Nil<>();
+        }
+
+        @Override
+        public <B> Seq<B> flatMap(Function<A, Seq<B>> func) {
+            return new Nil<>();
         }
     }
 }
--- java/src/main/java/io/github/ajoz/sequence/SeqTest.java
+++ java/src/main/java/io/github/ajoz/sequence/SeqTest.java
@@ -1,14 +1,25 @@
 package io.github.ajoz.sequence;
 
+import java.util.Arrays;
+
 public class SeqTest {
     private static Seq<Integer> fib(final int a, final int b) {
         return new Seq.Cons<>(a, () -> fib(b, a + b));
     }
 
     public static void main(String[] args) {
-        Seq<Integer> seq = fib(0, 1).take(20);
-        for (Integer integer : seq) {
-            System.out.println(integer);
+//        Seq<Integer> seq =
+////                fib(0, 1)
+////                        .take(5)
+////                        .map(i -> i * 42);
+////        for (Integer integer : seq) {
+////            System.out.println(integer);
+////        }
+        Seq<String> seq =
+                Seq.of(Arrays.asList("This", "is", "a", "test"))
+                    .flatMap(str -> Seq.of(Arrays.asList(str, str)));
+        for (String s : seq) {
+            System.out.println(s);
         }
     }
 }
